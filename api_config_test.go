911cb0c1cec5009fca802e574c7c2b21254c07f4
Ashwin-Rajeev <ashwinrajeev916@gmail.com>
2020-03-02 19:25:58 +0530

    Added unit test cases

--- api_config_test.go
+++ api_config_test.go
@@ -8,13 +8,10 @@ import (
 func Test_newAPIConfig(t *testing.T) {
 	type args struct {
 		goroutines      int
-		url             string
-		method          string
-		header          map[string]string
 		duration        int
-		body            string
 		timeOut         int
 		finalStatusChan chan *APIStatus
+		params          []*RequestParams
 	}
 	tests := []struct {
 		name string
@@ -25,30 +22,36 @@ func Test_newAPIConfig(t *testing.T) {
 			name: "test1",
 			args: args{
 				goroutines:      10,
-				url:             "www.sample.com",
-				method:          "POST",
-				header:          nil,
 				duration:        10,
-				body:            "sample",
 				timeOut:         1000,
 				finalStatusChan: nil,
+				params: []*RequestParams{
+					{
+						URL:    "www.sample.com",
+						Method: "POST",
+						Header: nil,
+					},
+				},
 			},
 			want: &APIConfig{
 				concurrentConnections: 10,
-				url:                   "www.sample.com",
-				method:                "POST",
-				header:                nil,
 				duration:              10,
-				body:                  "sample",
 				timeOut:               1000,
 				finalStatus:           nil,
 				interrupt:             0,
+				params: []*RequestParams{
+					&RequestParams{
+						URL:    "www.sample.com",
+						Method: "POST",
+						Header: nil,
+					},
+				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newAPIConfig(tt.args.goroutines, tt.args.url, tt.args.method, tt.args.header, tt.args.duration, tt.args.body, tt.args.timeOut, tt.args.finalStatusChan); !reflect.DeepEqual(got, tt.want) {
+			if got := newAPIConfig(tt.args.goroutines, tt.args.duration, tt.args.timeOut, tt.args.finalStatusChan, tt.args.params); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("newAPIConfig() = %v, want %v", got, tt.want)
 			}
 		})
